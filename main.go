902054cd3558457ab39b45011bb8a0001ea7a095
a.khisamutdinov <a.khisamutdinov@vkteam.ru>
2025-05-02 00:15:17 +0300

    init

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var shotsNum = 3
+var intervalDuration = 3
+
+var bot *tgbotapi.BotAPI
+var chatID = int64(5448179159)
+
+func sleepFor(d time.Duration) {
+	// time.Sleep(d)
+	bot.Send(tgbotapi.NewMessage(chatID, fmt.Sprintf("will sleep for %s", d.String())))
+}
+
+func main() {
+	token := os.Getenv("BOT_TOKEN")
+	var err error
+	bot, err = tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	now := time.Now()
+
+	todayAt13 := time.Date(
+		now.Year(),
+		now.Month(),
+		now.Day(),
+		13, 0, 0, 0,
+		now.Location(),
+	)
+
+	if time.Now().Hour() > 13 {
+		bot.Send(tgbotapi.NewMessage(chatID, "bot exited unsuccessfully, manual action required"))
+		return
+	}
+
+	for {
+		sleepFor(time.Until(todayAt13))
+		for range shotsNum {
+			sleepHours := rand.Intn(intervalDuration)
+			sleepMinutes := rand.Intn(60)
+			sleepFor(time.Duration(sleepHours)*time.Hour + time.Duration(sleepMinutes)*time.Minute)
+			bot.Send(tgbotapi.NewMessage(chatID, "shot"))
+		}
+		time.Sleep(10 * time.Second)
+	}
+}
